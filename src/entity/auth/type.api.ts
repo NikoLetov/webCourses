6a1a5b9baad65bc1e3776d8d9173b8937ae20566
NikoLetov <niko.letov@mail.ru>
2026-03-29 03:06:22 +0300

    11/adding-db-json

--- src/entity/auth/type.api.ts
+++ src/entity/auth/type.api.ts
@@ -1,4 +1,11 @@
-import { type UserType } from './data'
-
 export type AuthUserType = Pick<UserType, 'email' | 'password'>
 export type AuthSession = Omit<UserType, 'password'>
+export type UserRole = 'admin' | 'user'
+export type UserType = {
+	id: number
+	name: string
+	email: string
+	password: string
+	role: UserRole
+	avatar?: string
+}
