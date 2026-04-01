682223e6f139a701005e9e973963b3069743399c
NikoLetov <niko.letov@mail.ru>
2026-04-01 19:42:24 +0300

    Reapply "14/add-db-courses"
    This reverts commit 22eaab6eaa199c84c205ac12f880ed701bd97915.

--- src/routes/(app)/index.tsx
+++ src/routes/(app)/index.tsx
@@ -1,15 +1,22 @@
 import { Container } from '@/shared/ui/container'
 import { CoursesList } from '@/widget/coursesList'
 import { createFileRoute } from '@tanstack/react-router'
+import { Suspense } from 'react'
 
 export const Route = createFileRoute('/(app)/')({
 	component: CoursesPage
 })
 
+const data = fetch('http://localhost:3000/courses', {
+	method: 'GET'
+}).then((item) => item.json())
+
 function CoursesPage() {
 	return (
 		<Container>
-			<CoursesList />
+			<Suspense fallback={<div>Loading...</div>}>
+				<CoursesList data={data} />
+			</Suspense>
 		</Container>
 	)
 }
